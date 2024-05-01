ce6f57b3ecfe5e19baec8b80b3423f2a6df1ec84
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-05-01 23:18:46 +0300

    clean code

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
 use std::str::from_utf8;
-fn first_word(s: &String) -> &[u8]{
- 
- let bytes: &[u8] = s.as_bytes();
- for (i, &item) in bytes.iter().enumerate() {
-  if item == b' ' {
-  println!("found a space at position : {}",i);
- let slice=&bytes[0..i];
- return slice;
- }
- ;
-
- }
-let slice=&bytes[..];
-  slice
+fn first_word(s: &String) -> &[u8] {
+    let bytes: &[u8] = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            println!("found a space at position : {}", i);
+            let slice = &bytes[0..i];
+            return slice;
+        }
+    }
+    let slice = &bytes[..];
+    println!("no space found got the whole slice as the word");
+    slice
 }
 fn main() {
-
-    
-       match from_utf8(first_word(& "magic's world!".to_string())){
-        Ok(word)=>println!("this is the first word: {}",word),
-        Err(err)=>println!("error: {}",err)
-       }
-   
-      }
+    match from_utf8(first_word(&"magic's world".to_string())) {
+        Ok(word) => println!("this is the first word: {}", word),
+        Err(err) => println!("error: {}", err),
+    }
+}
