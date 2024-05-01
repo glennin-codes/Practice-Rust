5bd9c57b8223ef5abfec4e05bb058361a469eec9
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-05-01 22:04:49 +0300

    get the first word in a string

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::str::from_utf8;
+fn first_word(s: &String) -> &[u8]{
+ 
+ let bytes = s.as_bytes();
+ for (i, &item) in bytes.iter().enumerate() {
+  if item == b' ' {
+  println!("found a space at position : {}",i);
+ let slice=&bytes[0..i];
+ return slice;
+ }
+ ;
+
+ }
+let slice=&bytes[..];
+  slice
+}
+fn main() {
+
+    
+       match from_utf8(first_word(& "magic's world!".to_string())){
+        Ok(word)=>println!("this is the first word: {}",word),
+        Err(err)=>println!("error: {}",err)
+       }
+   
+      }
